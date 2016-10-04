dffc60678475e8ed3b114f63539b7fd8802ad7cf
exstalis <exstalis@gmail.com>
2016-10-04 13:35:30 +0300

    first commit

---
+++ PlayLocalShow/VideoCell.swift
@@ -0,0 +1,24 @@
+//
+//  VideoCell.swift
+//  PlayLocalShow
+//
+//  Created by elif ece arslan on 10/4/16.
+//  Copyright © 2016 elif ece arslan. All rights reserved.
+//
+
+import UIKit
+
+class VideoCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
